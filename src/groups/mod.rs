a3732ce13cbf5567d7ffd6eaa8958fb7fb53595b
David Ruescas <david@sequentech.io>
2025-06-11 16:27:42 +0200

    wip: p256

--- src/groups/mod.rs
+++ src/groups/mod.rs
@@ -1,3 +1,5 @@
 pub mod ristretto255;
+pub mod p256;
 
 pub use ristretto255::Ristretto255Group;
+pub use p256::P256Group;
--- src/utils/hash.rs
+++ src/utils/hash.rs
@@ -1,8 +1,13 @@
-use sha3::{Digest, Sha3_512}; // Or your chosen default digest algorithm
+use sha3::{Digest, Sha3_512, Sha3_256}; // Or your chosen default digest algorithm
 
 /// Returns a new hasher instance of the library's default digest algorithm.
 pub fn new_default_hasher() -> Sha3_512 {
     Sha3_512::new()
+}
+
+/// Used by p256:hash_to_scalar
+pub fn new_256_hasher() -> Sha3_256 {
+    Sha3_256::new()
 }
 
 /// Updates a given hasher with multiple byte slices.
